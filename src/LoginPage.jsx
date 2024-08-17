be9a3588d05abbdc3f54a6ea8c85fa03fc2c2275
Prabath Samarasinghe <prabhaths2001@gamil.com>
2024-08-17 13:51:53 +0530

    Modified the login page with CSS

--- src/LoginPage.jsx
+++ src/LoginPage.jsx
@@ -5,31 +5,62 @@ function LoginPage(){
         <div id="mainblock">
             <div className="box" id="login">
                 <h1>Login</h1><br />
-                <input type="text" placeholder="username"/>
-                <i class='bx bx-user'></i><br />
-                <input type="password" placeholder="Password"/>
-                <i class='bx bx-lock-alt'></i><br />
-                <button className ="buttons" type="submit">Login</button>
-                <button className ="buttons" type="reset">Clear</button>
+                <div className="details">
+                    <input type="text" placeholder="username"/>
+                    <i class='bx bx-user'></i>
+                </div>
+    
+                <div className="details">
+                    <input type="password" placeholder="Password"/>
+                    <i class='bx bx-lock-alt'></i>
+                </div>
+                
+                <div className="log">
+                    <button className ="Abuttons" type="submit">Login</button>
+                    <button className ="Abuttons" type="reset">Clear</button>
+                </div>
+               
+                <div className="checkBox">
+                    <label htmlFor="Remember"><input type="checkbox" id="Remember"/>Remember me</label>
+                    <a href="">Forgot password?</a>
+                </div>
+                
             </div>
             <div className="box" id="signin">
                 <h1>Sign in</h1>
-                <input type="text" placeholder="username"/>
-                <i class='bx bx-user'></i><br />
-                <input type="password" placeholder="Enter your password"/>
-                <i class='bx bx-lock-alt'></i><br />
-                <input type="password" placeholder="Re enter your password"/>
-                <i class='bx bx-lock-alt'></i><br />
-                <button className ="buttons" type="submit">Login</button>
-                <button className ="buttons" type="reset">Clear</button><br /><br />
-                <button className ="buttons" type="submit">Login from google</button>
-                <i class='bx bxl-google'></i><br />
-                <button className ="buttons" type="submit">Login from facebook</button>
-                <i class='bx bxl-facebook-circle' ></i><br />
-                <button className ="buttons" type="submit">Login from linkedin</button>
-                <i class='bx bxl-linkedin-square' ></i><br />
-                
+                <div className="details">
+                    <input type="text" placeholder="username"/>
+                    <i class='bx bx-user'></i>
+                </div>
 
+                <div className="details">
+                    <input type="password" placeholder="Enter your password"/>
+                    <i class='bx bx-lock-alt'></i>
+                </div>
+                
+                <div className="details">
+                    <input type="password" placeholder="Re enter your password"/>
+                    <i class='bx bx-lock-alt'></i>
+                </div>
+                
+                <div className="log">
+                    <button className ="Abuttons" type="submit">Login</button>
+                    <button className ="Abuttons" type="reset">Clear</button>
+                </div>
+                <div className="con">
+                    <div className="Btn">
+                        <button className ="buttons" type="submit">Login from google</button>
+                        <i class='bx bxl-google'></i>
+                    </div>
+                    <div className="Btn">
+                        <button className ="buttons" type="submit">Login from facebook</button>
+                        <i class='bx bxl-facebook-circle' ></i>
+                    </div>
+                    <div className="Btn">
+                        <button className ="buttons" type="submit">Login from linkedin</button>
+                        <i class='bx bxl-linkedin-square' ></i>
+                    </div>
+                </div>
             </div>
         </div>
     );
